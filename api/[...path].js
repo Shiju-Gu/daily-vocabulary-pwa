e958f71d7e73cbbc515c19f861ea7492dc503064
Shiju-Gu <shijugu2023@gmail.com>
2026-07-04 12:37:46 -0700

    Delete api directory

--- api/[...path].js
+++
@@ -1,6 +0,0 @@
-const serverHandler = require("../server");
-const handleRequest = serverHandler.handleRequest || serverHandler;
-
-module.exports = function vocabularyApi(request, response) {
-  return handleRequest(request, response);
-};
