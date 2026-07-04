9b7e85acb0caf9b55f9e198ff613ead244977ebe
Shiju-Gu <shijugu2023@gmail.com>
2026-07-04 16:32:18 -0700

    Delete api directory

--- api/meaning.js
+++
@@ -1,6 +0,0 @@
-const serverHandler = require("./_server");
-const handleRequest = serverHandler.handleRequest || serverHandler;
-
-module.exports = function meaningApi(request, response) {
-  return handleRequest(request, response);
-};
