1c37cb4efe020450163db9a8d77115da3f44528b
Shiju-Gu <shijugu2023@gmail.com>
2026-07-04 16:36:07 -0700

    Delete api directory

--- api/pronunciation.js
+++
@@ -1,6 +0,0 @@
-const serverHandler = require("./_server");
-const handleRequest = serverHandler.handleRequest || serverHandler;
-
-module.exports = function pronunciationApi(request, response) {
-  return handleRequest(request, response);
-};
