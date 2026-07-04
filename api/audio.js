5d6e8cd93d30e9e7a31988db5549897c05effd4e
Shiju-Gu <shijugu2023@gmail.com>
2026-07-04 16:27:32 -0700

    Delete api directory

--- api/audio.js
+++
@@ -1,6 +0,0 @@
-const serverHandler = require("./_server");
-const handleRequest = serverHandler.handleRequest || serverHandler;
-
-module.exports = function audioApi(request, response) {
-  return handleRequest(request, response);
-};
